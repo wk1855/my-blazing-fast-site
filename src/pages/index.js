6297c67d8ed497ace5ebf93e3b7f00dfc2ea4aeb
王杰 <893437648@qq.com>
2020-08-31 14:48:54 +0800

    加入博客部署的功能

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,14 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>你好呀</h1>
     <p>欢迎来到我的博客</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <p>我就想说下</p>
+    <br />
+    <h1>我喜欢的人儿是康康</h1>
+    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
   </Layout>
 )
 
