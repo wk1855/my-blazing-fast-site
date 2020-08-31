81565a389b86c0f62bcb9f6126f149e6cd818c41
王杰 <37725239+wangjie1855@users.noreply.github.com>
2020-08-31 16:32:28 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
     <h4>欢迎来到我的博客</h4>
     <h3>我就想说下</h3>
     {/* <h1>我喜欢的人儿是康康</h1> */}
-    <h1><Link to="/page-2/">点开我看~</Link> <br /></h1>
+    <h1><Link to="/my-blazing-fast-site/page-2/">点开我看~</Link> <br /></h1>
     {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
